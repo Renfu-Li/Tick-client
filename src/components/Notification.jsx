b93a94ccfb0febcefd0debfc0112263e5bb37a62
Renfu Li <renfu.li@outlook.com>
2024-01-26 10:16:05 -0800

    fix: notification turns to the opposite color when sliding down

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,15 +1,24 @@
 import { Alert, Slide } from "@mui/material";
 import CheckCircleOutlineIcon from "@mui/icons-material/CheckCircleOutline";
 import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 
 function Notification() {
   const notification = useSelector((state) => state.notification);
+  const [severity, setSeverity] = useState("success");
+
+  useEffect(() => {
+    if (notification.length > 0) {
+      const newSeverity = notification[0] === "S" ? "success" : "error";
+      setSeverity(newSeverity);
+    }
+  }, [notification]);
 
   return (
     <Slide direction="up" in={Boolean(notification)} mountOnEnter unmountOnExit>
       <Alert
         icon={<CheckCircleOutlineIcon />}
-        severity={notification[0] === "S" ? "success" : "error"}
+        severity={severity}
         variant="filled"
         style={{
           position: "absolute",
